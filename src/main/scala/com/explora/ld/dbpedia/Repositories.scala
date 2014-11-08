d7b10fcd6fce4e2863ff67b01a2ed3a8127ef237
Fred CECILIA <fred@pc2.home>
2014-11-09 00:08:23 +0100

    New management of dbpedias repo

--- src/main/scala/com/explora/ld/dbpedia/Repositories.scala
+++ src/main/scala/com/explora/ld/dbpedia/Repositories.scala
@@ -1,18 +1,45 @@
 package com.explora.ld.dbpedia
 
-import com.explora.model.Repository
+import com.explora.model.{Entity, Repository}
 
 /**
  * Created by fred on 15/03/2014.
  */
-trait DBPedia {
 
-  implicit val dbpediaorg = Repository("http://dbpedia.org/sparql")
+
+trait DBTrait {
+
+  def uri_base: String
+  def uri_base_resource: String
+  implicit lazy val repository = Repository(repo_uri)
+
+  def repo_uri: String = uri_base + "/sparql"
+
+  def uri(s: String) = uri_base_resource + "/resource/" + s
+
+  def DBEntity(s: String) = Entity(uri(s))
+
 
 }
 
-trait DBPediaFr {
+trait DBPedia extends DBTrait {
 
-  implicit val frdbpediaorg = Repository("http://fr.dbpedia.org/sparql")
+  val uri_base = "http://dbpedia.org"
+  val uri_base_resource = "http://dbpedia.org"
+
+}
+
+trait DBPediaFr extends DBTrait {
+
+  val uri_base = "http://fr.dbpedia.org"
+  val uri_base_resource = "http://fr.dbpedia.org"
+
+}
+
+trait LiveDBPedia extends DBTrait {
+
+  val uri_base = "http://live.dbpedia.org"
+  val uri_base_resource = "http://dbpedia.org"
+
 
 }
