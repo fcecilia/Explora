85fcdccb4ff29be98ec588fa1a45c8acffdae491
Fred CECILIA <fred@pc2.home>
2014-11-09 13:30:28 +0100

    New Herler

---
+++ src/main/scala/com/explora/pattern/ExploraHelper.scala
@@ -0,0 +1,52 @@
+package com.explora.pattern
+
+import com.explora.model.{Literal, RDFNode, Entity, Repository}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+
+/**
+ * Created by fred on 24/03/2014.
+ */
+object ExploraHelper {
+
+
+  implicit class StringRequest(s: String) {
+
+    def execute(implicit rep: Repository) = rep.execute(s)
+
+    def executeAndGet (variable: String)(implicit rep: Repository) = {
+      rep.execute(s).map(m =>m.get(variable))
+    }
+
+  }
+
+
+  implicit class String2Entity(s: String)(implicit rep: Repository) {
+
+
+    def entity = Entity(s)
+
+  }
+
+
+  implicit class LiteralEntity(nodes: Stream[RDFNode])(implicit rep: Repository){
+
+    def onlyEntities =
+
+      nodes.map {
+        case Literal(value, opt) => None
+        case e:Entity => Some(e )
+      }.filter(m => m != None).map(_.get)
+
+
+    def onlyLiterals =
+
+      nodes.map {
+        case l:Literal => Some(l)
+        case e:Entity => None
+      }.filter(m => m != None).map(_.get)
+    }
+
+
+}
