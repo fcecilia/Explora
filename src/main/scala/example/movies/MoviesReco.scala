91a7a9ecf4cbb07af3c7535e37313a4303b09de3
Fred CECILIA <fred@pc2.home>
2014-11-09 11:50:42 +0100

    reformat

--- src/main/scala/example/movies/MoviesReco.scala
+++ src/main/scala/example/movies/MoviesReco.scala
@@ -11,13 +11,14 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * Created by fred on 08/11/2014.
  */
 object MoviesReco extends App with LiveDBPedia {
-
+  val smillis = System.currentTimeMillis()
 
   println(repository)
-//  val film = uri("Men_in_Black_(film)")
- // val film = uri("The_Terminator")
-  //val film = uri("Jurassic_Park_(film)")
-  val film = uri("Pretty_Woman")
+  val film = uri("The_Matrix")
+  //val film = uri("Men_in_Black_(film)")
+  //  val film = uri("The_Terminator")
+  // val film = uri("Jurassic_Park_(film)")
+  // val film = uri("Pretty_Woman")
 
   val request = s"""
 select distinct ?movies ?Concept where {
@@ -42,7 +43,10 @@ select distinct ?movies ?Concept where {
       case Entity(uri) => Some(uri)
     }.filter(m => m != None && m != Some(film.uri)).map(_.get)
 
-    val by: List[(String, Int)] = movies.groupBy(a => a).map(m => (m._1, m._2.size)).toList.sortBy(-_._2).take(5)
+    val by: List[(String, Int)] = movies.groupBy(a => a).map(m => (m._1, m._2.size)).toList.sortBy(-_._2).take(10)
+
+
+    println(System.currentTimeMillis() - smillis)
 
     by.foreach(println)
 
